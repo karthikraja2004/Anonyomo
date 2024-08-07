9e3d47846c0fc9f2a1b6d6ebf5ac2be2f2a11493
Vasanthan <srvasanthan33@outlook.com>
2024-08-07 21:36:38 +0530

    Created authentication, after login, token(user_id,username) will be stored in cookie

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,9 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
 
+const authenticationRoute = require('./routes/authentication')
+const { checkUser } = require('./middleware/authMiddleware')
+
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
@@ -23,11 +26,12 @@ app.get('/', (req, res) => {
     res.send("Backend Route")
 })
 
-app.get('/api/v1/profile', (req, res) => {
-    res.send("Profile page of User")
+// Protected route
+app.get('/api/v1/profile', checkUser, (req, res) => {
+    res.send(`Welcome to Profile \n ${req.token.username}`)
 })
 
-const authenticationRoute = require('./routes/authentication')
+
 app.use('/api/v1/auth', authenticationRoute)
 
 app.listen(PORT, () => console.log(`server started at http://127.0.0.1:${PORT}`))

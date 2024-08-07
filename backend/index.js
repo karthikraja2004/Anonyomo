dda0052858fabe189b6b0d4ed47d231f814fcec5
Vasanthan <srvasanthan33@outlook.com>
2024-08-07 19:22:11 +0530

    created database connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,17 @@
 const express = require('express')
-const PORT = 5500
 const app = express()
+
+require('dotenv').config()
+
+const PORT = 5500
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("Connection successful")
+    })
+    .catch((err) => console.log("database connection failed"))
+
 
 app.get('/', (req, res) => {
     res.send("Backend Route")

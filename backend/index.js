13b624b2706fd015296ad1a3e2a88854d1b068d9
Vasanthan <srvasanthan33@outlook.com>
2024-08-08 13:28:36 +0530

    Modified api/v1/ to api/ for api endpoint

--- backend/index.js
+++ backend/index.js
@@ -27,10 +27,10 @@ app.get('/', (req, res) => {
 })
 
 // auth route
-app.use('/api/v1/auth', authenticationRoute)
+app.use('/api/auth', authenticationRoute)
 
 // Protected route
-app.get('/api/v1/profile', checkUser, (req, res) => {
+app.get('/api/profile', checkUser, (req, res) => {
     res.send(`Welcome to Profile \n ${req.token.username}`)
 })
 

47be632bdbda60c5793563202fee813b1df87413
Vasanthan <srvasanthan33@outlook.com>
2024-08-08 22:19:40 +0530

    Created posts schema and updated category

---
+++ backend/models/post.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const postSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    }
+}, {
+    collection: 'posts',
+    timestamps: 'true'
+})
+module.exports = mongoose.model('post', postSchema)
